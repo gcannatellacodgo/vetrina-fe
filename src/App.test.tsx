17aa700eb10219b0d1317f87c9639376706f07af
GiggiaWo <160729501+GiggiaWo@users.noreply.github.com>
2024-02-28 18:59:34 +0100

    Registrazione

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -11,8 +11,4 @@ import '@mantine/core/styles.css';
 // import '@mantine/code-highlight/styles.css';
 // ...
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
