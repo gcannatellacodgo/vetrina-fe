31ec2afa7b02f372297d1385ecfa45265b892fe3
ericariccio <ericcio94@gmail.com>
2024-03-01 09:51:23 +0100

    Aggiunta copertina e carosello

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,49 @@
 import copertina from './immagini/ps5.jpg'
+import '@mantine/carousel/styles.css';
+import {Carousel} from "@mantine/carousel";
+import ps5Slim from './immagini/ps5_slim.jpg'
+import gameboy from './immagini/gameboy.jpg'
 export default function Home(){
     return(
-        <div className={"relative h-screen flex items-center justify-center overflow-hidden"}>
-            <img src={copertina} className="absolute inset-0 object-cover w-full"/>
+        <div>
+            <div className="w-full">
+                <img src={copertina} className="  w-full "/>
+            </div>
+
+
+            <div className={"p-10 flex space-x-3"}>
+                <p className={"font-semibold"}> In evidenza</p>
+
+                <div>
+                    <p className={"text-sky-400 hover:text-red-800 font-semibold"}>Visualizza tutti i prodotti</p>
+                </div>
+
+            </div>
+
+
+
+            <Carousel
+                withIndicators
+                height={200}
+                slideSize="33.333333%"
+                slideGap="md"
+                loop
+                align="start"
+                slidesToScroll={3}
+            >
+                <Carousel.Slide><img src={gameboy}/></Carousel.Slide>
+                <Carousel.Slide><img src={ps5Slim}/></Carousel.Slide>
+                <Carousel.Slide><img src={gameboy}/></Carousel.Slide>
+                <Carousel.Slide><img src={ps5Slim}/></Carousel.Slide>
+                <Carousel.Slide><img src={gameboy}/></Carousel.Slide>
+                <Carousel.Slide><img src={ps5Slim}/></Carousel.Slide>
+                <Carousel.Slide><img src={gameboy}/></Carousel.Slide>
+                {/* ...other slides */}
+            </Carousel>
+
+
+
 
         </div>
-
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Home from "./Home";
+import {MantineProvider} from "@mantine/core";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Home/>
+      <MantineProvider>
+        <Home/>
+      </MantineProvider>
   </React.StrictMode>
 );
 
